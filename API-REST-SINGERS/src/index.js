e8a14dfc4c9360f20adda11f33e8e20e0d65a102
Romulo Pardo <rapardo1s@gmail.com>
2023-02-17 11:24:37 -0500

    update

--- API-REST-SINGERS/src/index.js
+++ API-REST-SINGERS/src/index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 
 mongoose.set("strictQuery", true);
 const app = express();
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3013;
 
 app.use(cors());
 
